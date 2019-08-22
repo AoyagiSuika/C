83974ef97bc448771d4c3ff46e2ef0ad3d6aac9a
dahak_d <dahak.deborah@gmail.com>
2019-08-22 19:49:22 +0200

    Quelques exercices pour se remettre au C.

---
+++ Reprise/camthalion.c
@@ -0,0 +1,18 @@
+/**
+ * @Author: Dahak Déborah <suika>
+ * @Date:   22-08-2019
+ * @Email:  deborah.dahak@gmail.com
+ * @Project: Reprise
+ * @Filename: camthalion.c
+ * @Last modified by:   suika
+ * @Last modified time: 22-08-2019
+ */
+
+ #include <stdio.h>
+
+int main()
+ {
+    printf("Ma devise est 'Parler peu mais parler bien'.\n");
+    printf("Je m'appelle Camthalion\n");
+    printf("Coucou !\n");
+ }
