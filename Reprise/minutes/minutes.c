d7993400dfc0323fa73057efdce0f41715135499
dahak_d <dahak.deborah@gmail.com>
2019-08-23 18:29:06 +0200

    minutes.c

---
+++ Reprise/minutes/minutes.c
@@ -0,0 +1,24 @@
+/**
+ * @Author: Dahak Déborah <suika>
+ * @Date:   23-08-2019
+ * @Email:  deborah.dahak@gmail.com
+ * @Project: Reprise
+ * @Filename: minutes.c
+ * @Last modified by:   suika
+ * @Last modified time: 23-08-2019
+ */
+
+#include <stdio.h>
+
+void minutes(int *h, int *m) {
+  *h = *m/60;
+  *m = *m%60;
+  return ;
+}
+
+int main() {
+  int h = 0, m = 90;
+  minutes(&h, &m);
+  printf("%d heures et %d minutes.\n", h, m);
+  return 0;
+}
