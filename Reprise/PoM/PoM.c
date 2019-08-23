107096d6ff188c4c04681e1310f1ef66ba5c6441
dahak_d <dahak.deborah@gmail.com>
2019-08-23 13:18:49 +0200

    PoM.c

---
+++ Reprise/PoM/PoM.c
@@ -0,0 +1,38 @@
+/**
+ * @Author: Dahak Déborah <suika>
+ * @Date:   23-08-2019
+ * @Email:  deborah.dahak@gmail.com
+ * @Project: Reprise
+ * @Filename: PoM.c
+ * @Last modified by:   suika
+ * @Last modified time: 23-08-2019
+ */
+
+/* Ce petit jeu consiste à deviner un nombre aléatoire comptris entre
+** 1 et 100. A chaque proposition, le jeu vous indique si c'est plus
+** ou moins, ou si vous avez gagné.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  const int MAX = 100, MIN = 1;
+  int rep = 0;
+  int nb = 0;
+
+  srand(time(NULL));
+  nb = (rand() % (MAX - MIN + 1)) + MIN;
+
+    while (rep != nb) {
+      printf("Quel est le nombre ?\n");
+      scanf("%d", &rep);
+      if (rep < nb)
+	      printf("C'est plus !\n");
+      if (rep > nb)
+	      printf("C'est moins !\n");
+    }
+  printf("GG !\n");
+  return 0;
+}
