7e46b5df506b83a311ee1f288ef4986328ce8b8a
dahak_d <dahak.deborah@gmail.com>
2019-08-23 09:56:44 +0200

    hello_world.c

---
+++ Reprise/hello_world/hello_world.c
@@ -0,0 +1,16 @@
+/**
+ * @Author: Dahak Déborah <suika>
+ * @Date:   22-08-2019
+ * @Email:  deborah.dahak@gmail.com
+ * @Project: Reprise
+ * @Filename: hello_world.c
+ * @Last modified by:   suika
+ * @Last modified time: 23-08-2019
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello world!\n");
+  return 0;
+}
